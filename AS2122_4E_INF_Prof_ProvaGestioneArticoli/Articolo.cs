6124fde14e365822a9bfdd5cf0e811f89329a660
PC05 <PC05@INFO5-5376>
2022-03-16 09:41:15 +0100

    - (5) aggiungere attributi privati dei dati inseriti nella frmArticoli - 9:41

--- AS2122_4E_INF_Prof_ProvaGestioneArticoli/Articolo.cs
+++ AS2122_4E_INF_Prof_ProvaGestioneArticoli/Articolo.cs
@@ -18,6 +18,7 @@ namespace AS2122_4E_INF_Prof_ProvaGestioneArticoli
 
         // TODO: (9) aggiungere property di sola lettura dei dati necessari all'esterno di Articolo
         // ...
+
         public Articolo(int codice, string descrizione, string unitaMisura, double prezzo)
         {
             // TODO: (10) impostazione costruttore classe Articolo come utilizzato da frmMain
